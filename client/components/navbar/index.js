b379c388af3d1c6f70b9187fc71b32cf1e2ad1e9
retsambew <namanaskn2@gmail.com>
2022-05-29 23:21:32 +0530

    nav

--- client/components/navbar/index.js
+++ client/components/navbar/index.js
@@ -1,9 +1,24 @@
+import Image from 'next/image'
 import styles from '../../styles/navbar.module.css';
 
 const Navbar = () => {
   return (
-    <div>
-        <h1>Navbar</h1>
+    <div className={styles.container}>
+        <div className={styles.logo}>
+          {/* <a><Image src='/' width='10px' height='10px'></Image></a> */}
+        </div>
+
+        <div className={styles.nav}>
+          <ul>
+            <li>Home</li>
+            <li>Rental</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+
+        <div className={styles.btn}>
+          <button>Login</button>
+        </div>
     </div>
   )
 }
