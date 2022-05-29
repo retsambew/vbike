2bad1c87ef2e88defb465ade3dfc2d7c16d217ef
retsambew <namanaskn2@gmail.com>
2022-05-30 02:20:17 +0530

    temp

---
+++ client/components/home/Use.js
@@ -0,0 +1,22 @@
+import styles from '../../styles/home.module.css';
+
+const Use = () => {
+  return (
+    <div>
+        <h1>How it works</h1>
+
+        {/* image */}
+        <h2>Find a bicycle</h2>
+        <p>Hop on to the website and follow directions for renting a bicycle. 
+            Reserve a nearby stop and simply walk up to the vehicle to get started.</p>
+
+        <h2>Start riding</h2>
+        <p>Enter the OTP you have received to start your ride and you are good to go.</p>
+
+        <h2>End the ride responsibly</h2>
+        <p>To end your trip, park your bicycle at any nearby destination stop.</p>
+    </div>
+  )
+}
+
+export default Use
