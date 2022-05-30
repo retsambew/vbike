4f146ddd3c3a60ed84c03a4c7ec80acf325b652f
Nayan0403 <namanagarwal1407000@gmail.com>
2022-05-30 10:34:31 +0530

    Form

--- client/src/components/Auph/index.js
+++ client/src/components/Auph/index.js
@@ -8,15 +8,32 @@ const Auph = () => {
                 <div className="form">
 
                     <label>Enter Your Username</label>
-                    <input typeof="text"></input>
+                    <input type="text"></input>
                     <label> Enter Your Password </label>
 
-                    <input typeof="password"></input>
+                    <input type="password"></input>
                 </div>
             </form>
             <div className="buttons">
                 <button className="LogIn">Log In</button>
                 <button className="Create">Create New Account?</button>
+            </div>
+
+            <div className="signup_form">
+            <form method = "post">
+                <label>Enter Username</label>
+                <input type="text"></input>
+                <label>Enter password</label>
+                <input type="password"></input>
+                <label>Phone number</label>
+                <input type="phone-number"></input>
+                <label>Registration Number</label>
+                <input type = "text"></input>
+                <div className="buttons2">
+                    <button>Sign UP</button>
+                    <button>Log In</button>
+                </div>
+            </form>
             </div>
         </div>
     )
