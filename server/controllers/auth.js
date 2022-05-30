69859a561718f75f0650ae83b32407fe6669164c
retsambew <namanaskn2@gmail.com>
2022-05-30 07:23:29 +0530

    login

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,12 +1,29 @@
+import model from '../model/userInfo.js'
 
-export const login = (req,res,next) => {
-    res.send('login')
+export const login = async (req,res) => {
+    try{
+        const user= await model.find({username:req.body.username,password:req.body.password});
+        
+        res.status(200).json(user);
+    }catch(err){
+        res.status(404).json({message: err.message})
+    }
 }
 
 export const logout = (req,res) => {
 
 }
 
-export const signup = (req,res) => {
+export const signup = async (req,res) => {
+    const { username, password, phone, regno}=req.body;
 
+    const user=new model({ username, password, phone, regno});
+    try{
+        await user.save();
+
+        res.status(201).json(user);
+    }catch(err){
+        console.log(err.message);
+        res.status(409).json({message:err.message});
+    }
 }
