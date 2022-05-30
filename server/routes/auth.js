4d43453bcbf146a96623b7fd60cc7598579b74bf
retsambew <namanaskn2@gmail.com>
2022-05-30 05:39:38 +0530

    backend

---
+++ server/routes/auth.js
@@ -0,0 +1,10 @@
+import express from "express";
+import {login,logout,signup} from '../controllers/auth.js'
+
+const router= express.Router();
+
+router.get('/login',login);
+router.post('/logout',logout);
+router.post('/signup',signup);
+
+export default router;
